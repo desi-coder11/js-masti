5362809d34e6db7eabd69cea253b8e03abd572a2
desi-coder11 <65103107+desi-coder11@users.noreply.github.com>
2024-05-18 06:33:00 +0000

    "added conversionOperation"

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 12341;
+let name = "Krishna";
+var bank_name = "PNB";
+
+let accountName;
+
+console.table([accountId,name,bank_name,accountName]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+let number = 10;
+let string = "hello"
+let boolean = true;
+
+/*
+BigInt;
+null // standalone value
+undefined
+Symbol
+*/
+
+console.table([number,string,boolean])
